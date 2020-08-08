2a4af98ba0bda68f6a55c5ae5ac116c5328c9fc9
wubbyz <wtporte@outlook.com>
2020-08-07 21:43:09 -0500

    tinkering with ideas

---
+++ Server.py
@@ -0,0 +1,9 @@
+# Make Server
+# Functionality Wish list:
+# allow users to send and receive messages
+# Connect to server from anywhere/ maybe end up federating users/
+# serverless? prebaked ami?
+# end to end encryption
+# option for message to self delete on read
+# Bonus objective: integrate media project
+
